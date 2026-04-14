9e575b98b7c59f4976bc40654a388e3ba5a57eed
Gurpreet Sanjay Kukkar <kukkar@usc.edu>
2026-04-14 13:04:03 -0700

    Add: boats-to-save-people - submission-3

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        i, j = 0, len(people)-1
+        boat= 0
+        while i <= j:
+            current_weight= people[i]+people[j]
+            if current_weight > limit:                
+                j-=1
+            else:
+                i+=1
+                j-=1
+            boat+=1
+        return boat
+                
+
+
+                
+
