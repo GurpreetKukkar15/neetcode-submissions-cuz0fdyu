b0002620a488a292cf355803d55cc4aaa4834872
Gurpreet Sanjay Kukkar <kukkar@usc.edu>
2026-04-20 15:29:24 -0700

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r= 0, x
+        if x ==0:
+            return 0
+        while l < r:
+            mid= (l+r+1)//2
+            if mid*mid==x:
+                return mid
+            elif mid*mid< x:
+                l= mid 
+            elif mid*mid > x:
+                r= mid - 1
+        return l
+            
